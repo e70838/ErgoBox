917be7ebc3a7c0c5c7a9671139daacf70a880380
e70838 <e70838@gmail.com>
2019-05-30 18:56:27 +0200

    Initial commit

---
+++ app/src/test/java/com/example/ergobox/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.ergobox;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+        assertEquals("addition works", 2, 1+1);
+    }
+}
